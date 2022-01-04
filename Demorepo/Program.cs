6b5c289a2e8c7ba08403234e93db6971013b0c91
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-04 08:28:17 +0530

    modified

--- Demorepo/Program.cs
+++ Demorepo/Program.cs
@@ -1,9 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello");
-<<<<<<< HEAD
 Console.WriteLine("hi");
-=======
+Console.WriteLine("welcome");
 Console.WriteLine("how r u");
->>>>>>> cbfeed8cb62101d905e109c41bf6d5571051a521
 Console.ReadLine();
