2dd52ad9ae23677cdc45979b8185a4054b6747f2
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-04 09:37:06 +0530

    [mahafuz] added fibonacciSerires program

---
+++ Demorepo/FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demorepo
+{
+    public class FibonacciSeries
+    {
+        public int firstNum = 0, secondNum = 1, result = 1;
+        
+        public void findFibonacciSeries()
+        {
+            Console.WriteLine("Please enter number to generate series");
+          //string num = Console.ReadLine();
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("User number is: " +num);
+            result = firstNum + secondNum;
+            Console.Write("Series are : "+firstNum+ " " +secondNum);
+            for(int i=2;i<num;i++)
+            {
+                firstNum = secondNum;
+                secondNum = result;
+                Console.Write("\t" + result);
+                result = firstNum + secondNum;
+                
+            }
+        }
+    }
+}
--- Demorepo/Program.cs
+++ Demorepo/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Demorepo;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello");
 Console.WriteLine("hi");
 Console.WriteLine("welcome");
 Console.WriteLine("how r u");
+
+FibonacciSeries fs = new FibonacciSeries();
+fs.findFibonacciSeries();
+
 Console.ReadLine();
