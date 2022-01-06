bdfcb33170120da59f257a1996508ab51043d39c
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-06 17:35:23 +0530

    [mahafuz] added factorail program

---
+++ Demorepo/Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demorepo
+{
+    class Factorial
+    {
+        public static void findFact()
+        {
+            int i, number, fact;
+            Console.WriteLine("Enter the Number");
+            number = int.Parse(Console.ReadLine());
+            fact = 1;
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nFactorial of Given Number is: " + fact);
+            Console.ReadLine();
+
+        }
+    }
+}
