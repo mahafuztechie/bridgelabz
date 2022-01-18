8d9580784480dec805d5cb0696dc77f620272a09
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-18 14:38:40 +0530

    [mahafuz] adding assignment programs

---
+++ Demorepo/LargestOfThree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demorepo
+{
+    internal class LargestOfThree
+    {
+        public static void FindLargestOfThree()
+        {
+            Console.WriteLine("Enter a first num: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a second num: ");
+            int second = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a third num: ");
+            int third = int.Parse(Console.ReadLine());
+
+
+            if (first > second && first > third)
+            {
+                Console.WriteLine(first + " is  greater than " + second +" and "+ third);
+            }
+            else if(second > third)
+            {
+                Console.WriteLine(second + " is  greater than " + first + " and " + third);
+            }
+            else
+            {
+                Console.WriteLine(third + " is  greater than " + first + " and" + second);
+            }
+        }
+    }
+}
---
+++ Demorepo/VowelsAndConsonants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demorepo
+{
+    internal class VowelsAndConsonants
+    {
+        public static void FindVowelOrConsonant()
+        {
+            char alphabet;
+            Console.WriteLine("Enter a alphabet : ");
+            alphabet = Convert.ToChar(Console.ReadLine().ToLower());
+           
+            if (alphabet == 'A' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u')
+            {
+                Console.WriteLine(alphabet + " is vowel");
+            }
+            else
+            {
+                Console.WriteLine(alphabet + " is a consonant");
+            }
+        }
+    }
+}
