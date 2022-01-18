d6824721d7d8ad4f2ace150c5bec616898ccb739
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-18 14:46:16 +0530

    [mahafuz] adding assignment programs

---
+++ Demorepo/EvenOrOdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demorepo
+{
+    internal class EvenOrOdd
+    {
+        public static void findEvenOrOdd()
+        {
+            Console.WriteLine("Enter a num ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num <= 0)
+            {
+                Console.WriteLine("enter a positive num");
+            }
+            else if (num % 2 == 0)
+            {
+                Console.WriteLine("number is even : " +num);
+            }
+            else
+            {
+                Console.WriteLine("number is odd : " + num);
+            }
+        }
+    }
+}
---
+++ Demorepo/QuotientAndRemainder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demorepo
+{
+    internal class QuotientAndRemainder
+    {
+        public static void FindQuotientAndRemainder()
+        {
+            Console.WriteLine("Enter a dividend: ");
+            int dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a divisor: ");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient is : " +quotient);
+            Console.WriteLine("Remainder is : " +remainder);
+        }
+    }
+}
